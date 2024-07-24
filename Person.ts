693659c40f471969d040fcea7cb498ab4782549f
Yazzz28 <yacinemennaa@gmail.com>
2024-07-24 12:14:38 +0200

    quete 04

---
+++ Person.ts
@@ -0,0 +1,30 @@
+class Person
+{
+    private name: string;
+    private age: number;
+
+    constructor(name: string, age: number)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public tellMyName(): string
+    {
+        return 'I am ${this.name}';
+    }
+
+    public tellMyAge(): string
+    {
+        return 'I am ${this.age} years old';
+    }
+}
+
+
+let person = new Person('John', 40);
+console.log(person.tellMyName());
+console.log(person.tellMyAge());
+
+let person2 = new Person('Mary', 35);
+console.log(person2.tellMyName());
+console.log(person2.tellMyAge());
